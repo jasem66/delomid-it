110917458438c6fb7ea7eb1729d8587c0e5182a0
jasem66 <jasemsarafraz66@gmail.com>
2023-02-13 19:24:57 +0200

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,13 +3,25 @@ import './navbar.scss'
 import images from '../../utilities/images'
 import { HiSearch } from 'react-icons/hi'
 import { FiMenu } from 'react-icons/fi'
+import {navLinks} from '../../utilities/constants'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <section className='navbar'>
       <div className='nav-logo'>
-        <img src={images.logo} alt='Delomid-it' />
+        <Link to='/'>        <img src={images.logo} alt='Delomid-it' />
+</Link>
       </div>
-      <li className='nav-links'></li>
+      <ul className='nav-links'>
+        {navLinks.map((item) => {
+          const { id, title, path } = item
+          return (
+            <Link className='nav-link' to={path}>
+              {title}
+            </Link>
+          ) 
+        })}
+      </ul>
       <div className='nav-icons'>
         <div className='nav-toggle'>
           <FiMenu />
