25f7e87a964921ec79b1260e961991b55ccf5e15
jasem66 <jasemsarafraz66@gmail.com>
2023-02-13 14:48:56 +0200

    router

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import OurServices from './OurServices'
+import Career from './Career'
+import Contact from './Contact'
+import Blog from './Blog'
+
+export { Home, OurServices, Career, Contact, Blog }
