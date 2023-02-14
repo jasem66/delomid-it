eb954c559a8fe032323ea1b47dfcd288ace6af87
jasem66 <jasemsarafraz66@gmail.com>
2023-02-14 23:03:51 +0200

    cahnges

--- src/components/business/Business.jsx
+++ src/components/business/Business.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react'
 import './business.scss'
-import images from '../../utilities/images'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDatabase, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { businessData } from '../../data/bussinesData'
+import { businessData, digitalData } from '../../data/bussinesData'
 const Business = () => {
-  
-
   return (
     <div className='business'>
       <div className='business-row'>
@@ -33,8 +28,22 @@ const Business = () => {
 
       <div className='business-row'>
         <h3>digital-online</h3>
-        <p>Design of web and mobile applications</p>{' '}
+        <p>Design of web and mobile applications</p>
         <div className='seperator-dark'></div>
+
+        {digitalData.map((item) => {
+          const { id, icon, title, description } = item
+          return (
+            <div key={id} className='single-business'>
+              <div className='icon-container'>
+                <div className='overlay' />
+                <div className='icon'>{icon}</div>
+              </div>
+              <h3 className='title'>{title}</h3>
+              <p>{description}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
--- src/data/bussinesData.js
+++ src/data/bussinesData.js
@@ -1,8 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faChartBar,
+  faCode,
   faDatabase,
-  faEnvelope,
+  faPalette,
+  faPallet,
+  faPen,
+  faPenFancy,
+  faSearch,
+  faStore,
   faSuitcase,
   faThumbsUp,
 } from '@fortawesome/free-solid-svg-icons'
@@ -37,32 +43,37 @@ export const businessData = [
 export const digitalData = [
   {
     id: 1,
-    title: 'Data integration',
-    description: 'Synchronization, migration, consolidation',
-    icon: <FontAwesomeIcon className='font-icon' icon={faDatabase} />,
+    title: 'Creation of website and application',
+    description:
+      'We design your website by studying with you your objectives, your targets and the opportunities it should be able to generate.',
+    icon: <FontAwesomeIcon className='font-icon' icon={faCode} />,
   },
   {
     id: 2,
-    title: 'Data quality',
-    description: 'Monitoring, Standardization, Deduplication, Profiling',
-    icon: <FontAwesomeIcon className='font-icon' icon={faThumbsUp} />,
+    title: 'Web referencing and SEO',
+    description:
+      'Your company will appear in the first results on search engines if its structure is well optimized by experts.',
+    icon: <FontAwesomeIcon className='font-icon' icon={faSearch} />,
   },
   {
     id: 3,
-    title: 'Customer repository',
-    description: 'Single customer repository, Master Data Management',
-    icon: <FontAwesomeIcon className='font-icon' icon={faSuitcase} />,
+    title: 'Content writing',
+    description:
+      'Quality content has a positive impact on your SEO and writing web content must appeal to both Internet users and search engines.',
+    icon: <FontAwesomeIcon className='font-icon' icon={faPenFancy} />,
   },
   {
     id: 4,
-    title: 'Data visualization',
-    description: 'Process integration, service bus',
-    icon: <FontAwesomeIcon className='font-icon' icon={faChartBar} />,
+    title: 'internet marketing',
+    description:
+      'Developing your brand image, increasing your sales and energizing your network are very strategic issues in the development of your business.',
+    icon: <FontAwesomeIcon className='font-icon' icon={faStore} />,
   },
   {
-    id: 4,
-    title: 'Data visualization',
-    description: 'Process integration, service bus',
-    icon: <FontAwesomeIcon className='font-icon' icon={faChartBar} />,
+    id: 5,
+    title: 'Graphic design',
+    description:
+      'Your image on the web is a major asset of your success in the digital world.Our specialists will design your visual design which will then be the key to your communication.',
+    icon: <FontAwesomeIcon className='font-icon' icon={faPalette} />,
   },
 ]
