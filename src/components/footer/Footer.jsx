24f6e178da844d40a06b5bdebb85c439093cf288
jasem66 <jasemsarafraz66@gmail.com>
2023-02-14 12:45:11 +0200

    ch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,41 +2,51 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './footer.scss'
 import { navLinks } from '../../utilities/constants'
+import { MdEmail } from 'react-icons/md'
+import { AiFillPhone } from 'react-icons/ai'
 const Footer = () => {
   return (
     <section className='footer'>
-      <div className='footer-links'>
-        <ul className='nav-links'>
-          {navLinks.map((item) => {
-            const { id, title, path } = item
-            return (
-              <Link className='nav-link' to={path}>
-                {title}
-              </Link>
-            )
-          })}
-        </ul>
-        <div className='addresses'>
-          <address>
-            <a href='mailto:jasemsarafraz66@gmail.com'>
-              <span> +32 2 544 04 45</span> (Belgium)
-            </a>
-          </address>
-          <address>
-            <a href='   mailto:jasemsarafraz66@gmail.com'>
-              <span>+352 621 47 17 46</span> (Luxembourg)
-            </a>
-          </address>
-          <address>
-            <a href='   mailto:jasemsarafraz66@gmail.com'>
-              <span>info@delomid-it.com </span>
-            </a>
-          </address>
+      <article className='footer-row'>
+        <div className='footer-links'>
+          <ul className='nav-links'>
+            {navLinks.map((item) => {
+              const { id, title, path } = item
+              return (
+                <Link className='nav-link' to={path}>
+                  {title}
+                </Link>
+              )
+            })}
+          </ul>
+          <div className='addresses'>
+            <address>
+              <a href='mailto:jasemsarafraz66@gmail.com'>
+                <AiFillPhone className='icon' /> <span> +32 2 544 04 45</span>{' '}
+                (Belgium)
+              </a>
+            </address>
+            <address>
+              <a href='   mailto:jasemsarafraz66@gmail.com'>
+                <span>+352 621 47 17 46</span> (Luxembourg)
+              </a>
+            </address>
+            <address>
+              <a href='   mailto:jasemsarafraz66@gmail.com'>
+                <MdEmail className='icon' />
+
+                <span> info@delomid-it.com </span>
+              </a>
+            </address>
+          </div>
+          <div className='search-footer'>
+            <input type='text' placeholder='To research...' />
+          </div>
         </div>
-        <div className='search-footer'>
-          <input type='text' placeholder='To research...' />
-        </div>
-      </div>
+      </article>
+      <article className='sub-footer'>
+        <p>DELOMID IT AGENCY © 2022 All rights reserved.</p>
+      </article>
     </section>
   )
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hero.scss'
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <h3>Our values ​​and our culture</h3>
+      
+    </section>
+  )
+}
+
+export default Hero
