de433ea6740d0dcfc73106cb75c7038d92189c12
jasem66 <jasemsarafraz66@gmail.com>
2023-02-16 15:02:07 +0200

    nav-f

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,27 +8,32 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <section className='navbar'>
-      <div className='nav-logo'>
-        <Link to='/'>
-          <img src={images.logo} alt='Delomid-it' />
-        </Link>
-      </div>
-      <ul className='nav-links'>
-        {navLinks.map((item) => {
-          const { id, title, path } = item
-          return (
-            <Link className='nav-link' to={path}>
-              {title}
-            </Link>
-          )
-        })}
-      </ul>
-      <div className='nav-icons'>
-        <div className='nav-toggle'>
-          <FiMenu />
+      <div className='nav-container'>
+        <div className='nav-logo'>
+          <Link to='/'>
+            <img src={images.logo} alt='Delomid-it' />
+          </Link>
         </div>
-        <div className='nav-search'>
-          <HiSearch />
+
+        <div className='nav-row'>
+          <ul className='nav-links'>
+            {navLinks.map((item) => {
+              const { id, title, path } = item
+              return (
+                <Link className='nav-link' to={path}>
+                  {title}
+                </Link>
+              )
+            })}
+          </ul>
+        
+            <span className='nav-toggle'>
+              <FiMenu />
+            </span>
+            <span className='nav-search'>
+              <HiSearch />
+            </span>
+         
         </div>
       </div>
     </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,10 @@ import { Business, Hero, HomeVideo, Jobs, Portfolio } from '../components'
 const Home = () => {
   return (
     <div className='home'>
+      <Hero/>
+<Business/>
+<HomeVideo/>
+<Portfolio/>
       <Jobs />
     </div>
   )
