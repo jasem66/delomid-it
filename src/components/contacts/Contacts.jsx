80d300cbbfe63893fa175f24f81ad3029d6834c9
jasem66 <jasemsarafraz66@gmail.com>
2023-02-16 19:38:19 +0200

    ch

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './contacts.scss'
 import { FaFacebookF } from 'react-icons/fa'
 import { FaLinkedinIn } from 'react-icons/fa'
+import { AiOutlinePhone } from 'react-icons/ai'
+import { AiOutlineMail } from 'react-icons/ai'
+
 const Contacts = () => {
   return (
     <div className='contacts'>
@@ -13,12 +16,22 @@ const Contacts = () => {
             <FaFacebookF color='#3b5998' className='font-icon' />
           </div>
         </div>
-        <div className="seperator-container">
-<div className="seperator-down"></div>
+        <div className='seperator-container'>
+          <div className='seperator-down'></div>
         </div>
         <div className='contacts-row-2'>
           <h3>Contact information</h3>
-          <div className='icons'></div>
+          <div className='icons'>
+            <div className='icon'>
+              <AiOutlinePhone />
+            </div>
+            <span>+32 2 544 04 45</span>
+            <article></article>
+            <div className='icon'>
+              <AiOutlineMail />
+            </div>
+            <span>info@delomid-it.com</span>
+          </div>
         </div>
       </div>
     </div>
