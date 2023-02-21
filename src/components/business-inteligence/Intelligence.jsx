cbd8f70055389437e1668ac811157c3c67fb3ac6
jasem66 <jasemsarafraz66@gmail.com>
2023-02-21 16:23:20 +0200

    changes

--- src/components/business-inteligence/Intelligence.jsx
+++ src/components/business-inteligence/Intelligence.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './intelligence.scss'
+import { jobsData } from '../../data/jobsData'
 const Intelligence = () => {
   return (
-    <div>Intelligence</div>
+    <div className='intelligence'>
+      <div className='overlay'></div>
+      <div className='intelligence-header'>
+        <h2>Bussines Intelligence</h2>
+      </div>
+
+      <div className='intelligence-row'>
+        <div className='intelligence-col'>
+          {jobsData.map((item) => {
+            const { id, title, date, desc } = item
+            return (
+              <div className='single-job' key={id}>
+                <h3>{title}</h3>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/our-services/Services.jsx
+++ src/components/our-services/Services.jsx
@@ -9,13 +9,14 @@ const Services = () => {
 
         <div className='services-col'>
           {servicesData.map((item) => {
-            const { id, title, desc, icon } = item
+            const { id, title, desc, icon,color } = item
 
             return (
               <div className='single-service'>
-                <div className='icon'>{icon}</div>
+                <div className='icon' style={{color:`#${color}`}}>{icon}</div>
                 <h3>{title}</h3>
                 <div className='desc-wrapper'>
+              
                   {desc.map((item) => {
                     return <p>{item}</p>
                   })}
