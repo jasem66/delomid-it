c9fc1940f5cd4e9f2659ea60451474f98039a44a
jasem66 <jasemsarafraz66@gmail.com>
2023-02-27 17:45:19 +0200

    Update Slider.jsx

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,59 +1,112 @@
 import React, { useEffect, useState } from 'react'
-import { sliderData } from '../../data/sliderData'
 import './slider.scss'
-import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
+import { sliderData } from '../../data/sliderData'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx'
+
 const Slider = () => {
-  const [imageIndex, setImageIndex] = useState(0)
-  const [data, setData] = useState(sliderData)
-      const lastImage = data.length - 1
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [winSize, setWinSize] = useState(window.innerWidth)
+  const [animeOn, setAnimeOn] = useState(false)
 
-  const increase = () => {
-    setImageIndex((prev) => prev + 1)
-  }
-  const decrease = () => {
-    setImageIndex((prev) => prev - 1)
+  const handlePrevClick = () => {
+    const index = currentIndex === 0 ? sliderData.length - 1 : currentIndex - 1
+    setCurrentIndex(index)
+    setAnimeOn(false)
   }
 
+  const handleNextClick = () => {
+    const index = currentIndex === sliderData.length - 1 ? 0 : currentIndex + 1
+    setCurrentIndex(index)
+    setAnimeOn(true)
+  }
+  const goToSlide = (slide) => {
+    setCurrentIndex(slide)
+  }
+
+  const handleSize = () => {
+    setWinSize(window.innerWidth)
+  }
   useEffect(() => {
-    if (imageIndex < 0) {
-      setImageIndex(lastImage)
+    window.addEventListener('resize', handleSize)
+  }, [])
+
+  const RenderCards = () => {
+    if (winSize > 1200) {
+      // Display 4 cards in large screens
+      return sliderData
+        .slice(currentIndex, currentIndex + 4)
+        .map((card, index) => (
+          <div
+            key={index}
+            className={animeOn ? 'card right-anime-f' : 'card left-anime-f'}
+          >
+            <img src={card} alt='' />
+          </div>
+        ))
     }
-    if (imageIndex > lastImage) {
-      setImageIndex(0)
+    if (winSize > 768) {
+      // Display 4 cards in large screens
+      return sliderData
+        .slice(currentIndex, currentIndex + 3)
+        .map((card, index) => (
+          <div
+            key={index}
+            className={animeOn ? 'card right-anime-f' : 'card left-anime-f'}
+          >
+            <img src={card} alt='' />
+          </div>
+        ))
     }
-  }, [imageIndex])
+    if (winSize > 599) {
+      // Display 4 cards in large screens
+      return sliderData
+        .slice(currentIndex, currentIndex + 2)
+        .map((card, index) => (
+          <div
+            key={index}
+            className={animeOn ? 'card right-anime-f' : 'card left-anime-f'}
+          >
+            <img src={card} alt='' />
+          </div>
+        ))
+    } else {
+      // Display 1 card in small screens
+      return (
+        <div className={animeOn ? 'card right-anime-f' : 'card left-anime-f'}>
+          <img src={sliderData[currentIndex]} alt='' />
+        </div>
+      )
+    }
+  }
 
   return (
-    <div className='slider'>
-    <h2> {imageIndex} </h2>
-      <div className='slider-container'>
-        <div className='img-wrapper'>
-          <div className='slider-btn'>
-            <button className='slider-left' onClick={decrease}>
-              <BiChevronLeft size={30} />
-            </button>
-            <button className='slider-right' onClick={increase}>
-              <BiChevronRight size={30} />
-            </button>
-          </div>
-          {/* <img src={data[index]} alt='' />
-          <img src={data[index + 1]} alt='' />
-          <img src={data[index + 2]} alt='' /> */}
-
-          {sliderData.map((item, index) => {
-            let position = 'right';
-            if(imageIndex===index){
-             position='center'
-            }
-            if(imageIndex=== index-1 || index === 0 && imageIndex === lastImage){
-             position = 'left'
-            }
-
-            return <img className={position} key={index} src={item} alt='' />
-          })}
+    <div className='card-carousel'>
+      <div className='card-container'>
+        <RenderCards />
+        <div className='button-container'>
+          <button className='prev-btn' onClick={handlePrevClick}>
+            <BsChevronLeft color='#222' size={30} />
+          </button>
+          <button className='next-btn' onClick={handleNextClick}>
+            <BsChevronRight color='#222' size={30} />
+          </button>
         </div>
+      </div>
+      <div className='dots'>
+        {sliderData.map((item, index) => {
+          if (index === currentIndex) {
+            return <RxDotFilled
+              size={50}
+              color='#d81414'
+              onClick={() => goToSlide(index)}
+            />
+          }
+          return <RxDotFilled size={50} onClick={() => goToSlide(index)} />
+        })}
       </div>
     </div>
   )
 }
+
 export default Slider
